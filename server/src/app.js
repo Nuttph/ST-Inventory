878e0f2218f246c993e1e675ea5196550770fd33
Nuttaphon <nathn2420@gmail.com>
2026-02-24 02:24:18 +0700

    update

--- server/src/app.js
+++ server/src/app.js
@@ -1,3 +1,4 @@
+// project: ST-Inventory
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
@@ -10,7 +11,7 @@ connectDB();
 const app = express();
 
 app.use(cors());
-app.use(express.json()); 
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('API is 1 running...');
